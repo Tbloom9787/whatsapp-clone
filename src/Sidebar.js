8b7a7d5fc41a35e77a60e5bc6c9a79b6dd2887f5
tbloom9787 <tylerbloom9787@gmail.com>
2020-09-09 16:38:54 -0700

    Add header with clickable icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton, Icon } from "@material-ui/core";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>This is the sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar alt="T" src="/images/profile.jpg" />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }
