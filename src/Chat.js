58639840c70f5c374820165317627ee27fab8bfc
tbloom9787 <tylerbloom9787@gmail.com>
2020-09-09 18:33:08 -0700

    Add chat header and icons

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import "./Chat.css"
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>This is the chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                
+            </div>
         </div>
     );
 }
