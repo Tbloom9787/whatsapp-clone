34087321d58b8d13d81b4069edb2b722713b93c9
tbloom9787 <tylerbloom9787@gmail.com>
2020-09-09 19:02:52 -0700

    Add messaging template for UI display

--- src/Chat.js
+++ src/Chat.js
@@ -27,7 +27,29 @@ function Chat() {
                 </div>
             </div>
             <div className="chat__body">
-                
+                <p className="chat__message">
+                    <span className="chat__name">Tyler</span>
+                        This is a message
+                    <span className="chat__timestamp">
+                        { new Date().toUTCString() } 
+                    </span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Tyler</span>
+                        This is a message
+                    <span className="chat__timestamp">
+                        { new Date().toUTCString() } 
+                    </span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">Tyler</span>
+                        This is a message
+                    <span className="chat__timestamp">
+                        { new Date().toUTCString() } 
+                    </span>
+                </p>
             </div>
         </div>
     );
