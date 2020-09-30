a536e347353eae4ba4efd369c5f3eee4742403ed
Tyler Bloom <tylerbloom9787@gmail.com>
2020-09-29 22:06:09 -0700

    Add mongo cluster connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 // Config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 // Middleware
 
 // DB Config
+const connection_url = 'mongodb+srv://admin:K57HRmKvLqWoPQS2@cluster0.ueg2j.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Routes
 app.get('/', (req,res)=>res.status(200).send('hello world'));
